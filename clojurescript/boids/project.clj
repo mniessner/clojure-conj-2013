363b437bcb393bd496760ebb27d7a992f4228076
Michael Niessner <mniessner@Michaels-MacBook-Air-2.local>
2013-11-11 13:19:04 -0500

    Lab: The browser REPL

--- clojurescript/boids/project.clj
+++ clojurescript/boids/project.clj
@@ -4,7 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1978"]]
+                 [org.clojure/clojurescript "0.0-1978"]
+                 [ring/ring-jetty-adapter "1.1.1"]
+                 [compojure "1.1.5"]]
+  :source-paths ["src/clj"]
+  :main boids.server
   :plugins [[lein-cljsbuild "0.3.4"]]
   :cljsbuild {:builds [{:source-paths ["src/cljs"]
                         :compiler {:optimizations :none
---
+++ clojurescript/boids/src/cljs/boids/brepl.cljs
@@ -0,0 +1,7 @@
+(ns boids.brepl
+  (:require [clojure.browser.repl :as repl]))
+
+(defn connect-repl
+  "Connects the browser REPL"
+    []
+    (repl/connect "http://localhost:9000/repl"))
