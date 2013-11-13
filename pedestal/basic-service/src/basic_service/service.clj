34abc228a486f7728d88a1fe7d5d8dc015b3edf3
Michael Niessner <mniessner@Michaels-MacBook-Air-2.local>
2013-11-13 11:27:53 -0500

    Lab: Interceptors, Routing, and Linking

---
+++ pedestal/basic-service/src/basic_service/service.clj
@@ -0,0 +1,63 @@
+(ns basic-service.service
+    (:require [io.pedestal.service.http :as bootstrap]
+              [io.pedestal.service.http.route :as route]
+              [io.pedestal.service.http.body-params :as body-params]
+              [io.pedestal.service.http.route.definition :refer [defroutes]]
+              [io.pedestal.service.interceptor :refer (defbefore defafter defhandler)]
+              [io.pedestal.service.log :as log]
+              [ring.util.response :as ring-resp]))
+
+(defn about-page
+  [request]
+  (ring-resp/response (format "Clojure %s - served from %s"
+                              (clojure-version)
+                              (route/url-for ::about-page))))
+
+(defn home-page
+  [request]
+  (ring-resp/response "Hello World!"))
+
+(defbefore log-request [context]
+  (log/info :request (:request context))
+    context)
+
+(defafter log-response [context]
+  (log/info :response (:response context))
+    context)
+
+(defhandler basic
+  [req]
+    (ring-resp/response "Response!"))
+
+
+(defroutes routes
+  [[["/" {:get home-page}
+         ^:interceptors [log-request log-response]
+              ["/basic" {:get basic}]]]])
+
+;; Consumed by basic-service.server/create-server
+;; See bootstrap/default-interceptors for additional options you can configure
+(def service {:env :prod
+              ;; You can bring your own non-default interceptors. Make
+              ;; sure you include routing and set it up right for
+              ;; dev-mode. If you do, many other keys for configuring
+              ;; default interceptors will be ignored.
+              ;; :bootstrap/interceptors []
+              ::bootstrap/routes routes
+
+              ;; Uncomment next line to enable CORS support, add
+              ;; string(s) specifying scheme, host and port for
+              ;; allowed source(s):
+              ;;
+              ;; "http://localhost:8080"
+              ;;
+              ;;::bootstrap/allowed-origins ["scheme://host:port"]
+
+              ;; Root for resource interceptor that is available by default.
+              ::bootstrap/resource-path "/public"
+
+              ;; Either :jetty or :tomcat (see comments in project.clj
+              ;; to enable Tomcat)
+              ;;::bootstrap/host "localhost"
+              ::bootstrap/type :jetty
+              ::bootstrap/port 8080})
